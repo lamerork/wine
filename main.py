c5cbe5fe0f6a74e0d9c0941ecd6e775cf962291d
Anton Ozerov <lamerork@gmail.com>
2023-07-05 01:08:50 +0300

    Оптимизировал код

--- main.py
+++ main.py
@@ -2,26 +2,23 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
-from pprint import pprint
 import collections
 import argparse
 
 YEAR_COMPANY_FOUNDATTION = 1920
 
+
 def get_ending(year):
-    
     num = year % 100
-    if num > 4 and num < 21: 
+    if num > 4 and num < 21:
         return 'лет'
-    
     num = num % 10
-    if num == 1: 
+    if num == 1:
         return 'год'
-    
-    if num > 1 and num < 5: 
+    if num > 1 and num < 5:
         return 'года'
-    
     return 'лет'
+
 
 def main():
 
@@ -38,12 +35,15 @@ def main():
     lifetime = datetime.datetime.now().year - YEAR_COMPANY_FOUNDATTION
 
     for wine in wines:
-        group_wines[wines[wine]['Категория']].append({'title': wines[wine]['Название'], 
-                                                     'category': wines[wine]['Категория'],
-                                                     'sort': wines[wine]['Сорт'],
-                                                     'price': wines[wine]['Цена'],
-                                                     'image': wines[wine]['Картинка'],
-                                                     'offer': wines[wine]['Акция']})
+        group_wines[wines[wine]['Категория']].append(
+                            {
+                                'title': wines[wine]['Название'],
+                                'category': wines[wine]['Категория'],
+                                'sort': wines[wine]['Сорт'],
+                                'price': wines[wine]['Цена'],
+                                'image': wines[wine]['Картинка'],
+                                'offer': wines[wine]['Акция']
+                            })
 
     env = Environment(
         loader=FileSystemLoader('.'),
@@ -53,17 +53,17 @@ def main():
     template = env.get_template('template.html')
 
     rendered_page = template.render(
-        age = lifetime,
-        ending = get_ending(lifetime),
-        group_wines = group_wines,
-        
-    )
+        age=lifetime,
+        ending=get_ending(lifetime),
+        group_wines=group_wines,
+        )
 
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
     server = HTTPServer(('127.0.0.1', 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
+
 
 if __name__ == '__main__':
     main()
